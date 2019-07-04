561f8f07ed02c3a7b43fa521d453aa6826cc566b
Maksim Hvatov <maksim.hvatov@smail.th-koeln.de>
2019-07-04 16:54:56 +0200

    Integration of the wagon maven plugin for uploading and debugging a java application

---
+++ src/main/java/org/makerclub/raspberrydeployment/Main.java
@@ -0,0 +1,9 @@
+package org.makerclub.raspberrydeployment;
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello Raspberry");
+        System.out.println("Noch eine Zeile");
+    }
+}
